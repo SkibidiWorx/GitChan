776349ffa950376cefb1712a53b1388584f09e51
limoncatt <75332327+limoncatt@users.noreply.github.com>
2025-01-12 13:04:20 +0100

    UPD4

--- tcbase.js
+++ tcbase.js
@@ -1,1 +1,5 @@
 document.getElementById("jsindicator").innerHTML = "If you see this text js is live. Imageboards should work correctly.";
+
+function acceptrules(rulesaccepted){
+    document.cookie = rulesaccepted;
+}

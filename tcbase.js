b60bdd894bc7dd24d01fc8ab54275e927f916e22
limoncatt <75332327+limoncatt@users.noreply.github.com>
2025-01-12 19:13:41 +0100

    B-added

--- tcbase.js
+++ tcbase.js
@@ -1,5 +1,1 @@
 document.getElementById("jsindicator").innerHTML = "If you see this text js is live. Imageboards should work correctly.";
-
-function acceptrules(rulesaccepted){
-    document.cookie = rulesaccepted;
-}
